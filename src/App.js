88345b5feef9a0301ca8a496bda9ce5f9930496f
floxy narteh <floxy.narteh@thecobaltpartners.com>
2024-05-29 00:16:40 +0200

    changes to src folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import { CiSearch } from "react-icons/ci";
 
-function App() {
+const API_URL = "http://www.omdbapi.com?apikey=e477f8ae ";
+
+const movie1 = {
+  Title: "Superman Returns",
+  Year: "2006",
+  imdbID: "tt0348150",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BNDUzZGRhNzktYTZkMC00YWFiLTljMDEtMTk2OWJhYzAyYmY2XkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg",
+};
+
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+    // return data;
+  };
+
+  useEffect(() => {
+    searchMovies("Superman");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <CiSearch />
+        {/* alt="search" onClick={() => {}} */}
+
+        <div className="container">
+          <div className="movie">
+            <div>
+              <p>{movie1.Year}</p>
+            </div>
+            <div>
+              <img
+                src={
+                  movie1.Poster !== "N/A"
+                    ? movie1.Poster
+                    : "https://via.placeholder.com/400"
+                }
+                alt={movie1.Title}
+              />
+            </div>
+            <div>
+              <span>{movie1.Type}</span>
+              <h3>{movie1.Title}</h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
